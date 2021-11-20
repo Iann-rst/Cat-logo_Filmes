9472215c3f028696eb6e65e3ae4224cfa22200a3
Iann-rst <iannrst11@gmail.com>
2021-11-20 18:27:37 -0300

    Página Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,41 @@
+/**
+ * Fazer um requisição http (api.get())
+ * useEffect: toda fez que abrir a página, busca os filmes na api
+ * Pegar apenas o id, nome e imagem dos filmes
+ */
+
+import api from '../../services/api'
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+export default function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      //sujeitoprogramador.com/r-api/?api=filmes
+      const response = await api.get('r-api/?api=filmes')
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.nome}</strong>
+              <img src={filme.foto} alt={filme.nome} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
